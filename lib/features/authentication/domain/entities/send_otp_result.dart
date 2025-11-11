e712e27700c343eda04aaf489d02887f6754d22e
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-11-11 13:35:34 +0530

    feat: Add @immutable annotation to SendOtpResult classes and update meta dependency

--- lib/features/authentication/domain/entities/send_otp_result.dart
+++ lib/features/authentication/domain/entities/send_otp_result.dart
@@ -1,8 +1,10 @@
+import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
 
 /// Result for the sendOtp flow.
 /// CodeSent indicates a verificationId was sent.
 /// Initiated indicates the verification process started (no verificationId available yet).
+@immutable
 abstract class SendOtpResult extends Equatable {
   const SendOtpResult();
 
@@ -10,6 +12,7 @@ abstract class SendOtpResult extends Equatable {
   List<Object?> get props => [];
 }
 
+@immutable
 class CodeSent extends SendOtpResult {
   final String verificationId;
   const CodeSent(this.verificationId);
@@ -18,6 +21,7 @@ class CodeSent extends SendOtpResult {
   List<Object?> get props => [verificationId];
 }
 
+@immutable
 class Initiated extends SendOtpResult {
   const Initiated();
 
